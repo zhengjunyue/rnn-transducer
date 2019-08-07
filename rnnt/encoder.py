3752fd117ef84bf635ba989404c48cbc7b82adaf
junjunjiang <45668655+zhengjunyue@users.noreply.github.com>
2019-08-07 22:58:02 +0100

    Update encoder.py

--- rnnt/encoder.py
+++ rnnt/encoder.py
@@ -23,23 +23,23 @@ class BaseEncoder(nn.Module):
         assert inputs.dim() == 3
 
         if input_lengths is not None:
-            sorted_seq_lengths, indices = torch.sort(input_lengths, descending=True)
+            sorted_seq_lengths, indices = torch.sort(input_lengths, descending=True) # (按行降序排列)
             inputs = inputs[indices]
-            inputs = nn.utils.rnn.pack_padded_sequence(inputs, sorted_seq_lengths, batch_first=True)
+            inputs = nn.utils.rnn.pack_padded_sequence(inputs, sorted_seq_lengths, batch_first=True) # 返回一个PackedSequence 对象
 
         self.lstm.flatten_parameters()
         outputs, hidden = self.lstm(inputs)
 
         if input_lengths is not None:
-            _, desorted_indices = torch.sort(indices, descending=False)
-            outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True)
+            _, desorted_indices = torch.sort(indices, descending=False)  # (按行升序排列)
+            outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True) # 把压紧的序列再填充回来， 返回的Varaible的值的size是 B×T×*
             outputs = outputs[desorted_indices]
 
         logits = self.output_proj(outputs)
 
         return logits, hidden
 
-
+# （160,320,320,4,0.3）
 def build_encoder(config):
     if config.enc.type == 'lstm':
         return BaseEncoder(
